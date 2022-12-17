8d6164f70b032f1b41828e0e749a93a461eefbfc
udita-22 <79441823+udita-22@users.noreply.github.com>
2022-12-17 23:17:14 +0530

    e

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,7 +61,7 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              <span className="text-color-primary">Voice Assistance for </span> Better Cause
+              <span className="text-color-primary">Voice Assistance </span> Better Cause For
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
